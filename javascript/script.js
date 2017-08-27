1fbc308b434b87d030b6b1471209d670f61f1405
Web Dev <universalhobbyist@gmail.com>
2017-08-26 23:12:55 -0400

    added first character button to page for testing, created two CSS classes

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+//creating character objects
+$(document).ready(function(){
+
+	var char1 = {
+
+		name: "Character 1",
+		health: 120,
+		attack: 10,
+		counter: 5,	
+
+		// draw/create clickable character box, maybe make it a button variable 
+		// have button/character default to character div
+		//one a character is clicked have all the other characters that were not clicked moved to the enemies row
+
+	}
+
+	var char2 = {
+
+		name: "Character 2",
+		health: 100,
+		attack: 5,
+		counter: 2,		
+	}
+
+	var char3 = {
+
+		name: "Character 3",
+		health: 150,
+		attack: 15,	
+		counter: 10,	
+	}
+
+	var char4 = {
+
+		name: "Character 4",
+		health: 180,
+		attack: 20,
+		counter: 15,		
+	}
+
+
+
+
+}
+
